14781675dbe997c3519dee75f8001f0a94da9d09
Michiya Takahashi <michiya.takahashi@gmail.com>
2013-12-12 18:52:02 +0900

    update the version to 0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ CLASSIFIERS=[
 
 setup(
     name='azure-storage-logging',
-    version='0.1.0',
+    version='0.1.1',
     description='Logging handlers that send logging output to Windows Azure Storage',
     long_description=open('README.rst').read(),
     author='Michiya Takahashi',
